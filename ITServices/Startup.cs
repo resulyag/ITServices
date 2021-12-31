f06ab1a5d078e7bad5738de9b6d2eb61d082f143
resulyag <resulyag@gmail.com>
2021-12-31 15:57:12 +0300

    sıfırdan mvc yapısı oluşturuyoruz

--- ITServices/Startup.cs
+++ ITServices/Startup.cs
@@ -16,6 +16,7 @@ namespace ITServices
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +29,17 @@ namespace ITServices
 
             app.UseRouting();
 
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                // 1. Yol
+                //  endpoints.MapDefaultControllerRoute();
+                // 2. Yol
+                endpoints.MapControllerRoute(
+                    name: "ITService",
+                    pattern: "{controller:Home}/{action:Index}/{id?}"
+                    );
             });
         }
     }
